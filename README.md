0b523eb0880f736189520d0bbd585f62cf7ff28c
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-10-25 11:37:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Beyond Bedrock 1.20.1
+ A mod that adds all the ores used in Flame's mods
---
+++ src/main/java/flamepoint1544/beyond_bedrock/BeyondBedrockDataGenerator.java
@@ -0,0 +1,11 @@
+package flamepoint1544.beyond_bedrock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeyondBedrockDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
