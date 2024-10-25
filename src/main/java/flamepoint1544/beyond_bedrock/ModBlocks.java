6c124d8b20b84fad5d7c232f47718899c265607e
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-10-25 12:23:15 -0600

    added initial things

---
+++ src/main/java/flamepoint1544/beyond_bedrock/ModBlocks.java
@@ -0,0 +1,32 @@
+package flamepoint1544.beyond_bedrock;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static final Block ULTIMATIUM_ORE = register(new Block(AbstractBlock.Settings.create()), "ultimatium_ore", true);
+    public static final Block ULTIMATIUM_RAW = register(new Block(AbstractBlock.Settings.create()), "ultimatium_raw_block", true); 
+    public static final Block ULTIMATIUM = register(new Block(AbstractBlock.Settings.create()), "ultimatium_block", true);
+
+    public static Block register(Block block, String id, boolean registerItem){
+        // Create Identifier
+        Identifier blockID = Identifier.of(BeyondBedrock.MOD_ID, id);
+
+        // check to see if it should register an item
+        if(registerItem){
+            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+            Registry.register(Registries.ITEM, blockID, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, blockID, block);
+    }
+
+    public static void initialize(){
+        
+    }
+}
---
+++ src/main/java/flamepoint1544/beyond_bedrock/ModItems.java
@@ -0,0 +1,37 @@
+package flamepoint1544.beyond_bedrock;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    // Registry keys
+    public static final RegistryKey<ItemGroup> PRIMARY_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(BeyondBedrock.MOD_ID, "primary_item_group"));
+
+    // Item Groups
+    public static final ItemGroup PRIMARY_ITEM_GROUP = FabricItemGroup.builder()
+    .icon(() -> new ItemStack(Items.BEDROCK))
+    .displayName(Text.translatable("itemGroup.beyond_bedrock.primary_item_group")).build();
+    
+    public static Item register(Item item, String id){
+        // Create Identifier
+        Identifier itemID = Identifier.of(BeyondBedrock.MOD_ID, id);
+
+        // Register the item
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return
+        return registeredItem;
+    }
+
+    public static void initialize(){
+        Registry.register(Registries.ITEM_GROUP, PRIMARY_GROUP_KEY, PRIMARY_ITEM_GROUP);
+    }
+}
