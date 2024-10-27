04ca2983f121910a8642fd84bd263df8be709cf9
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-10-26 19:29:31 -0600

    added models and fixed item group and hoe item

--- src/main/java/flamepoint1544/beyond_bedrock/ModItems.java
+++ src/main/java/flamepoint1544/beyond_bedrock/ModItems.java
@@ -1,6 +1,7 @@
 package flamepoint1544.beyond_bedrock;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.Item;
@@ -25,14 +26,14 @@ public class ModItems {
     public static final Item ULTIMATIUM_SHOVEL = register(new ShovelItem(ToolMaterials.ULTIMATIUM, 1.5f, -3.0f, new Item.Settings().rarity(Rarity.EPIC)), "ultimatium_shovel");
     public static final Item ULTIMATIUM_PICKAXE = register(new PickaxeItem(ToolMaterials.ULTIMATIUM, 1, 2.8f, new Item.Settings().rarity(Rarity.EPIC)), "ultimatium_pickaxe");
     public static final Item ULTIMATIUM_AXE = register(new AxeItem(ToolMaterials.ULTIMATIUM, 6, -3.2f, new Item.Settings().rarity(Rarity.EPIC)), "ultimatium_axe");
-    public static final Item ULTIMATIUM_HOE = register(new HoeItem(ToolMaterials.ULTIMATIUM, 0, -3, null), "ultimatium_hoe");
+    public static final Item ULTIMATIUM_HOE = register(new HoeItem(ToolMaterials.ULTIMATIUM, 0, -3, new Item.Settings().rarity(Rarity.EPIC)), "ultimatium_hoe");
 
     // Registry keys
     public static final RegistryKey<ItemGroup> PRIMARY_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(BeyondBedrock.MOD_ID, "primary_item_group"));
 
     // Item Groups
     public static final ItemGroup PRIMARY_ITEM_GROUP = FabricItemGroup.builder()
-    .icon(() -> new ItemStack(Items.BEDROCK))
+    .icon(() -> new ItemStack(Items.BEDROCK.asItem()))
     .displayName(Text.translatable("itemGroup.beyond_bedrock.primary_item_group")).build();
     
     public static Item register(Item item, String id){
@@ -48,5 +49,19 @@ public class ModItems {
 
     public static void initialize(){
         Registry.register(Registries.ITEM_GROUP, PRIMARY_GROUP_KEY, PRIMARY_ITEM_GROUP);
+
+         ItemGroupEvents.modifyEntriesEvent(PRIMARY_GROUP_KEY).register(itemGroup ->{
+            itemGroup.add(ULTIMATIUM);
+            itemGroup.add(ULTIMATIUM_RAW);
+            itemGroup.add(ULTIMATIUM_CORE);
+            itemGroup.add(ULTIMATIUM_SWORD);
+            itemGroup.add(ULTIMATIUM_PICKAXE);
+            itemGroup.add(ULTIMATIUM_AXE);
+            itemGroup.add(ULTIMATIUM_SHOVEL);
+            itemGroup.add(ULTIMATIUM_HOE);
+            itemGroup.add(ModBlocks.ULTIMATIUM);
+            itemGroup.add(ModBlocks.ULTIMATIUM_ORE);
+            itemGroup.add(ModBlocks.ULTIMATIUM_RAW);
+         });
     }
 }
